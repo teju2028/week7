177078150bfb6a300ac9fd30c92b7201a345ed92
teju <durgatejaswanikolli@gmail.com>
2026-08-27 14:17:15 +0530

    Add week 7 work

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("register.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    name = request.form["name"]
+    studentid = request.form["studentid"]
+    email = request.form["email"]
+    year = request.form["year"]
+
+    return render_template(
+        "success.html",
+        name=name,
+        studentid=studentid,
+        email=email,
+        year=year
+    )
+
+app.run(debug=True)
